d3a3e37f7d848c70a35b9a02fa2749446498202d
khum <1241367422@qq.com>
2018-08-31 17:53:24 +0800

    yzk update

--- src/main/java/com/yzk/controller/TestController.java
+++ src/main/java/com/yzk/controller/TestController.java
@@ -6,8 +6,15 @@ import com.yzk.util.ResponseUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/test")
@@ -26,10 +33,25 @@ public class TestController {
         return ResponseUtil.success(testService.test());
     }
 
-
     @GetMapping("/account")
     public String test3(){
         return "hello world";
+    }
+
+    @PostMapping("/test3")
+    public String test4(HttpServletRequest request){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(request.getInputStream(), "utf-8"));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = bufferedReader.readLine())!=null){
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
